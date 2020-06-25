eb4f35c109db21f25b2c32e258efb9ab2fad8675
Bernard Maltais <bmaltais@gmail.com>
2020-06-25 15:03:14 +0000

    Fix local issue

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   env_4                         = substr(var.env, 0, 4)
   serverType_3                  = substr(var.serverType, 0, 3)
   postfix_3                     = substr(var.postfix, 0, 3)
-  userDefinedString_7           = substr(var.userDefinedString, 0, 7 - length(postfix_3))
+  userDefinedString_7           = substr(var.userDefinedString, 0, 7 - length(local.postfix_3))
   vm-name                       = replace("${local.env_4}${local.serverType_3}-${local.userDefinedString_7}${local.postfix_3}", local.windows_virtual_machine_regex, "")
   plan                          = var.plan == null ? [] : [var.plan]
   boot_diagnostic               = var.boot_diagnostic ? ["1"] : []
