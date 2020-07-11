7a8eac424c4b538fef019a5e9b6dc0c0f965e31c
Bernard Maltais <bmaltais@gmail.com>
2020-07-11 17:14:26 +0000

    Add support for asg association

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "name" {
   depends_on = [azurerm_linux_virtual_machine.VM[0]]
-  value = azurerm_linux_virtual_machine.VM[0].name
+  value      = azurerm_linux_virtual_machine.VM[0].name
 }
 
 output "id" {
   depends_on = [azurerm_linux_virtual_machine.VM[0]]
-  value = azurerm_linux_virtual_machine.VM[0].id
+  value      = azurerm_linux_virtual_machine.VM[0].id
 }
 output "vm" {
   depends_on = [azurerm_linux_virtual_machine.VM[0]]
-  value = azurerm_windows_virtual_machine.VM[0]
+  value      = azurerm_windows_virtual_machine.VM[0]
 }
 
 output "pip" {
   depends_on = [azurerm_public_ip.VM-EXT-PubIP[0]]
-  value = azurerm_public_ip.VM-EXT-PubIP[0]
+  value      = azurerm_public_ip.VM-EXT-PubIP[0]
 }
 
 output "nic" {
   depends_on = [azurerm_network_interface.NIC[0]]
-  value = azurerm_network_interface.NIC[0]
+  value      = azurerm_network_interface.NIC[0]
 }
